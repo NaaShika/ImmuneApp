796e80428a06e4d31ca6c1eb8be95ecde4ec3795
NaaShika <haurielle@gmail.com>
2017-04-13 01:23:14 +0100

    added some new fragments; emergency contacts and doctor profiles

--- app/src/main/java/com/tutorial/aurie/finalyearproject/ChldView.java
+++ app/src/main/java/com/tutorial/aurie/finalyearproject/ChldView.java
@@ -2,6 +2,10 @@ package com.tutorial.aurie.finalyearproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ChldView extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class ChldView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chld_view);
+
+        ImageView imageView =(ImageView) findViewById(R.id.profileImageChild);
+        EditText editTextName =(EditText) findViewById(R.id.editTextChildName);
+        EditText editTextAge =(EditText) findViewById(R.id.editTextChildAge);
+        EditText editTextDOB =(EditText) findViewById(R.id.editTextChildDOB);
+        EditText editTextParentNumber=(EditText) findViewById(R.id.editTextParentNumber);
+        EditText editTextParentEmail=(EditText) findViewById(R.id.editTextParentEmail);
+        CheckBox checkBoxFemale =(CheckBox) findViewById(R.id.checkboxFemale);
+        CheckBox checkBoxMale =(CheckBox) findViewById(R.id.checkboxMale);
+        CheckBox checkBoxNotification =(CheckBox) findViewById(R.id.checkboxNotification);
+        Button buttonSaveChild = (Button)findViewById(R.id.buttonSaveChild);
+
     }
 }
