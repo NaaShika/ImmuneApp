cf9008837d6809d49a96fda2f53744e9fa91c039
NaaShika <haurielle@gmail.com>
2017-04-21 22:09:11 +0100

    dr profile info intent

--- app/src/main/java/com/tutorial/aurie/finalyearproject/DoctorProfile.java
+++ app/src/main/java/com/tutorial/aurie/finalyearproject/DoctorProfile.java
@@ -2,6 +2,7 @@ package com.tutorial.aurie.finalyearproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DoctorProfile extends AppCompatActivity {
@@ -12,5 +13,9 @@ public class DoctorProfile extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_profile);
 
         TextView textView = (TextView) findViewById(R.id.textViewDocName);
+        TextView textViewDocInfor = (TextView) findViewById(R.id.textViewDocInfor);
+        TextView textViewMeetingTime = (TextView) findViewById(R.id.textViewMeetingTime);
+        ImageView imageViewCallDr = (ImageView) findViewById(R.id.imageViewCallDr);
+        ImageView imageViewEmailDr = (ImageView) findViewById(R.id.imageViewEmailDr);
     }
 }
